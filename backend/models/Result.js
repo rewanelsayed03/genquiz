3119d9b96403e52eb75a48b5aa5016228732f3f4
Rewan Elsayed <rewan.elsayed2003@gmail.com>
2025-09-14 18:51:52 +0300

    Result.js

--- backend/models/Result.js
+++ backend/models/Result.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const resultSchema = new mongoose.Schema({
     answers: { type: [String], required: true },
-    score: { type: Number, required: true }, // optional, or any result metric
+    score: { type: Number, required: true }, 
     createdAt: { type: Date, default: Date.now }
 });
 
